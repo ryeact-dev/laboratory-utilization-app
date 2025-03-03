9046142823b50655935aed73d9f5ed515e848222
ryeact-dev <rmontoya.umtc@gmail.com>
2025-03-03 21:20:50 +0800

    Rearrange: Update Dean's Office Recieved Esign and User

--- client/src/common/sheets/right-side-bar/labratory-reports/weekly-utilizations/components/subjectWeeklyUtilizations/SubjectWeeklyUtilizations.jsx
+++ client/src/common/sheets/right-side-bar/labratory-reports/weekly-utilizations/components/subjectWeeklyUtilizations/SubjectWeeklyUtilizations.jsx
@@ -61,11 +61,43 @@ function SubjectWeeklyUtilizations({
       {whatToDisplay === "data" && (
         <article
           ref={componentToPrintRef}
-          className="forPrint font-sans text-gray-900"
+          className="forPrint relative font-sans text-gray-900"
         >
+          <div className="absolute -top-3 right-0 w-full max-w-72">
+            {singleReport && singleReport?.dean_acknowledged && (
+              <div className="w-full border-2 border-[#444484] p-1">
+                <div className="relative flex w-full flex-col items-start border-[1px] border-dashed border-[#444484] px-4 py-1 text-[#444484]">
+                  <div className="w-full">
+                    <p className="text-lg font-bold uppercase">Received:</p>
+                  </div>
+                  <div className="absolute right-0 top-0">
+                    <img
+                      src={eSign("Gina Fe G. Israel")}
+                      className="h-14 w-24 object-contain object-center"
+                      alt="esign"
+                    />
+                  </div>
+                  <p className="text-base font-bold">
+                    {`Gina Fe G. Israel, Ed.d`}
+                  </p>
+                  <p className="-mt-1.5 text-base font-semibold">
+                    {`Dean of College`}
+                  </p>
+                  <p className="-mt-0.5 text-sm font-semibold">
+                    Date:{" "}
+                    {format(
+                      new Date(singleReport?.dean_acknowledged),
+                      "MMM dd, yyyy",
+                    )}
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
+
           <header
             htmlFor="university-header-1"
-            className="mt-2 flex w-full items-center justify-between gap-3"
+            className="mt-6 flex w-full items-center justify-between gap-3"
           >
             <div className="flex flex-[3]">
               <figure className="my-auto ml-4 w-36 text-center">
@@ -80,38 +112,9 @@ function SubjectWeeklyUtilizations({
                 </h3>
               </div>
             </div>
-
-            <div className="flex flex-[1.5] justify-end">
-              {singleReport && singleReport?.dean_acknowledged && (
-                <div className="w-full border-2 border-[#444484] p-1">
-                  <div className="flex w-full flex-col items-end border-[1px] border-dashed border-[#444484] px-2 py-1 text-[#444484]">
-                    <div className="-mb-6 w-full">
-                      <p className="text-xl font-semibold uppercase">
-                        Received:
-                      </p>
-                    </div>
-                    <img
-                      src={eSign("Jenny Rose Perocillo")}
-                      className="h-14 w-24 object-contain object-center"
-                      alt="esign"
-                    />
-                    <p className="-mt-6 text-sm font-semibold uppercase">
-                      {`J. R. Perocillo, Dean's Office`}{" "}
-                    </p>
-                    <p className="-mt-1 text-sm font-semibold">
-                      Date:{" "}
-                      {format(
-                        new Date(singleReport?.dean_acknowledged),
-                        "MMM dd, yyyy",
-                      )}
-                    </p>
-                  </div>
-                </div>
-              )}
-            </div>
           </header>
           {/* Divider */}
-          <div className="mb-2 mt-4 border-b border-t-8 border-gray-900 pt-1"></div>
+          <div className="mb-2 mt-6 border-b border-t-8 border-gray-900 pt-1"></div>
           {/* For Print */}
           {weekNumber && (
             <>
